40d1a0f22c1a6524a2f646a9dcc81f04eefacb26
Andrew Hunter <ahh@cs.washington.edu>
2012-11-17 18:56:22 -0800

    ghetto parsing

--- week1/src/main/main.go
+++ week1/src/main/main.go
@@ -6,7 +6,20 @@ import "sorter"
 import "os"
 
 func main() {
-	stuff := vec.MakeVector(0, 0)
-
-	sorter.Sort(stuff)
+	f, _ := os.Open(os.Args[1])
+	defer f.Close()
+	v := vec.MakeVector(0, 0)
+	for {
+		var i int
+	        n, _ := fmt.Fscanf(f, "%d,", &i)
+		if n == 0 {
+			n, _ = fmt.Fscanf(f, "%d", &i)
+			if n == 0 {
+				break
+			}
+		}
+		fmt.Printf("Got: %d\n", i)
+		v.Push_back(i)
+	}
+	sorter.Sort(v)
 }
